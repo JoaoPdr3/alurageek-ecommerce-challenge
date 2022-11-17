6153aa743d2bd4fb260dc10258f2f57e6e2776a2
João Moreira <joaopedromxxi@gmail.com>
2022-11-17 01:12:50 -0300

    Config swiper to work

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 2,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  breakpoints: {
+    500: {
+      slidesPerView: 3,
+
+    },
+  }
+});
+
 // Fuctions Menu Expanded
 function openMenu() {
   document.body.classList.add('menu-expanded')
